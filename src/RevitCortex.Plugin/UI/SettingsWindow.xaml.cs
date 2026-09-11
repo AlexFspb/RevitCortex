d025497093f42372a15c71703132e69219ae5a4b
AlexFspb <alexander.fomenkov@gmail.com>
2026-09-11 10:14:59 +0200

    Restore settings page navigation

--- src/RevitCortex.Plugin/UI/SettingsWindow.xaml.cs
+++ src/RevitCortex.Plugin/UI/SettingsWindow.xaml.cs
@@ -1,18 +1,32 @@
 using System.Windows;
+using System.Windows.Controls;
 
 namespace RevitCortex.Plugin.UI;
 
 public partial class SettingsWindow : Window
 {
+    private readonly GeneralSettingsPage _generalPage;
+    private readonly ToolsSettingsPage _toolsPage;
+    private bool _isInitialized;
+
     public SettingsWindow()
     {
         InitializeComponent();
-        ContentFrame.Navigate(new GeneralSettingsPage());
+
+        _generalPage = new GeneralSettingsPage();
+        _toolsPage = new ToolsSettingsPage();
+
+        ContentFrame.Navigate(_generalPage);
+        _isInitialized = true;
     }
 
-    private void General_Click(object sender, RoutedEventArgs e)
-        => ContentFrame.Navigate(new GeneralSettingsPage());
+    private void NavListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if (!_isInitialized) return;
 
-    private void Tools_Click(object sender, RoutedEventArgs e)
-        => ContentFrame.Navigate(new ToolsSettingsPage());
+        if (NavListBox.SelectedItem == GeneralItem)
+            ContentFrame.Navigate(_generalPage);
+        else if (NavListBox.SelectedItem == ToolsItem)
+            ContentFrame.Navigate(_toolsPage);
+    }
 }
